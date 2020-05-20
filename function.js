ac4724b6ebb96036248c1386197d38d528641db8
Bhargavjois <43719681+Bhargavjois@users.noreply.github.com>
2020-05-20 17:31:12 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
 var call = document.getElementById("Call");
 function Call(){
-    call.href = "responsive.html";
+    call.href = "https://www.bhargavjois.github.io/silver_pancake/responsive.html";
 }
